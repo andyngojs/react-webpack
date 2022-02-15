d73eb29b05fa9e37d03c1948f267bcb2aa6b84f7
andyngojs <dongngo.2000@gmail.com>
2022-02-16 00:17:14 +0700

    useImperativeHandle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./App.scss";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+import Video from "./Video";
 
 export default function App() {
+  const videoRef = useRef();
+
+  const playVideo = () => {
+    videoRef.current.play();
+  };
+
+  const pauseVideo = () => {
+    videoRef.current.pause();
+  };
+
   return (
     <div className="container">
       <h1 className="title-header">Todo</h1>
       <TodoInput />
       <TodoList />
+      <hr></hr>
+      <Video ref={videoRef} />
+      <button onClick={playVideo}>Play</button>
+      <button onClick={pauseVideo}>Pause</button>
     </div>
   );
 }
---
+++ src/Video.js
@@ -0,0 +1,19 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import video1 from "./videos/video-1.mp4";
+
+const Video = (props, ref) => {
+  const videoRef = useRef();
+
+  useImperativeHandle(ref, () => ({
+    play() {
+      videoRef.current.play();
+    },
+    pause() {
+      videoRef.current.pause();
+    },
+  }));
+
+  return <video ref={videoRef} src={video1} width="240" />;
+};
+
+export default forwardRef(Video);
