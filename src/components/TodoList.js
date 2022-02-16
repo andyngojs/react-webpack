51eaef39ef847f9607278cf385479b45ffffaff9
andyngojs <dongngo.2000@gmail.com>
2022-02-16 11:27:32 +0700

    learn css module

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,11 @@
+import styles from "./TodoList.module.css";
 import TodoItem from "./TodoItem";
 import { useStore } from "../hooks";
 
 const TodoList = () => {
   const [state] = useStore();
   return (
-    <div className="container-list">
+    <div className={styles.containerList}>
       {state.todos.map((todo, index) => {
         return <TodoItem key={index} todo={todo} id={index} />;
       })}
